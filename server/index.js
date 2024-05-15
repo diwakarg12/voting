3edcf1393a8e500d8e6d7176f2efa87201bc41cd
Diwakar Giri <diwakargiri23@gmail.com>
2024-05-15 12:08:21 +0530

    adding variables in .env file

--- server/index.js
+++ server/index.js
@@ -83,5 +83,5 @@ app.get('/results', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
